023da4d39e0c7066ccfad96672f0b8b004820407
bruno <skgoddns1@gmail.com>
2023-03-02 00:05:55 +0900

    feat: First Commit

---
+++ src/main/java/com/szs/assignment/configure/JwtTokenConfigure.java
@@ -0,0 +1,63 @@
+package com.szs.assignment.configure;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "jwt.token")
+public class JwtTokenConfigure {
+
+    private String header;
+
+    private String issuer;
+
+    private String clientSecret;
+
+    private int expirySeconds;
+    private int expiryRefreshSeconds;
+
+    public String getHeader() {
+
+        return header;
+    }
+
+    public void setHeader(String header) {
+
+        this.header = header;
+    }
+
+    public int getExpiryRefreshSeconds() {
+        return expiryRefreshSeconds;
+    }
+
+    public void setExpiryRefreshSeconds(int expiryRefreshSeconds) {
+        this.expiryRefreshSeconds = expiryRefreshSeconds;
+    }
+
+    public String getIssuer() {
+        return issuer;
+    }
+
+    public void setIssuer(String issuer) {
+        this.issuer = issuer;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+
+    public int getExpirySeconds() {
+        return expirySeconds;
+    }
+
+    public void setExpirySeconds(int expirySeconds) {
+        this.expirySeconds = expirySeconds;
+    }
+
+
+
+}
---
+++ src/test/java/com/szs/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szs.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
